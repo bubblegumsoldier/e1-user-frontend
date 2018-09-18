76216b5eadd7e524607d484633632d911f1fd838
Henry Muessemann <h.muessemann@gmx.de>
2018-09-18 15:34:49 +0200

    Tried out medication recommendation

--- src/app/components/medication-recommendation/medication-recommendation.component.ts
+++ src/app/components/medication-recommendation/medication-recommendation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'e1-medication-recommendation',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./medication-recommendation.component.css']
 })
 export class MedicationRecommendationComponent implements OnInit {
+
+  @Input() medicationRecommendation = null;
 
   constructor() { }
 
